6f6b32614d3afd678f14586775a723690e6b9c39
sanbondeveloper <sanbondeveloper@gmail.com>
2024-04-08 18:04:47 +0900

    ✨ 배포 준비

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ app.set('port', process.env.PORT || 3001);
 connect();
 
 // 개발 환경 - dev, 배포 환경 - combined
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
 // 정적 파일 제공, public/images/image.png -> 서버주소/images/image.png
 app.use('/', express.static(path.join(__dirname, 'public')));
 // req.body
@@ -23,18 +27,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // req.cookies, 쿠키는 클라이언트에서 위조하기 쉬우므로 비밀 키를 통해 만들어낸 서명을 쿠키 값에 붙임, req.signedCookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false, // 배포시 true
-    },
-    name: 'server-session',
-  })
-);
+const sessionOption = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false, // 배포시 true
+  },
+  name: 'server-session',
+};
+if (process.env.NODE_ENV === 'production') {
+  sessionOption.proxy = true;
+  // sessionOption.cookie.secure = true;
+}
+app.use(session(sessionOption));
 
 app.use('/api', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 
 const authRouter = require('./auth');
 
 const router = express.Router();
 
+router.use(
+  cors({
+    credentials: true,
+  })
+);
 router.use('/auth', authRouter);
 
 module.exports = router;
